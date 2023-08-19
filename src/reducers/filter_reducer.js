61e4f46800606f09e8fbefe0eb391e9a3af3e01d
Shantanu Sutar <shantanusutar17@gmail.com>
2023-08-20 01:04:39 +0530

    filter functionality - category , company , colors , price , shipping

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -8,6 +8,7 @@ import {
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
 } from "../actions";
+import Product from "../components/Product";
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
@@ -74,6 +75,7 @@ const filter_reducer = (state, action) => {
     const { text, category, company, color, price, shipping } = state.filters;
     let tempProducts = [...all_products];
     //  filtering
+    //text
 
     // if (text) {
     //   tempProducts = tempProducts.filter((product) => {
@@ -88,6 +90,38 @@ const filter_reducer = (state, action) => {
       const regex = new RegExp(wordToMatch, "gi");
       return temp.name.match(regex);
     });
+
+    //category
+    if (category !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      );
+    }
+
+    //company
+    if (company !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.company === company
+      );
+    }
+
+    //colors
+    if (color !== "all") {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color);
+      });
+    }
+
+    //price
+    tempProducts = tempProducts.filter((product) => product.price <= price);
+
+    //shipping
+    if (shipping) {
+      tempProducts = tempProducts.filter(
+        (product) => product.shipping === true
+      );
+    }
+
     return {
       ...state,
       filtered_products: tempProducts,
