b9516c9c382313b880856c0d52cdd4c2745d9df9
Shantanu Sutar <shantanusutar17@gmail.com>
2023-08-27 00:34:20 +0530

    creating AuthWrapper to access " /checkout " url directly

--- src/App.js
+++ src/App.js
@@ -11,33 +11,36 @@ import {
   About,
   Products,
   Private,
+  AuthWrapper,
 } from "./pages";
 import PrivateRoute from "./pages/PrivateRoute";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/cart" element={<Cart />} />
-        <Route exact path="/products" element={<Products />} />
-        <Route exact path="/products/:id" element={<SingleProduct />} />
-        <Route
-          exact
-          path="/checkout"
-          element={
-            <PrivateRoute>
-              <Checkout />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/products/:id" element={<SingleProduct />} />
+          <Route
+            exact
+            path="/checkout"
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,9 +5,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useUserContext } from "../context/user_context";
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const { myUser } = useUserContext();
+  const { user } = useAuth0();
 
-  if (!myUser) return <Navigate to={"/"} />;
+  if (!user) return <Navigate to={"/"} />;
 
   return children;
 };
